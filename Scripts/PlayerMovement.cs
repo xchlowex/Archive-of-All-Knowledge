49734738602db1396dd0df2c729e353d9963df3d
Chloe Chen <79347948+xchlowex@users.noreply.github.com>
2026-04-06 17:03:18 +0800

    Update PlayerMovement.cs

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -1,76 +1,3 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class PlayerMovement : MonoBehaviour
-// {
-//     [SerializeField] private Animator animator;
-//     // public GameObject bullet;
-//     private Rigidbody2D rb;
-//     private Vector2 v;
-
-//     // Start is called before the first frame update
-//     // void Start()
-//     // {
-        
-//     // }
-
-//     // // Update is called once per frame
-//     // void Update()
-//     // {
-//     //     rb = GetComponent<Rigidbody2D>();
-//     //     v= rb.velocity;
-//     //     v.x = Input.GetAxis("Horizontal") * 10;
-//     //     v.y = Input.GetAxis("Vertical") * 10;
-//     //     rb.velocity = v;
-//     //     /* if (Input.GetKeyDown("space"))
-//     //     {
-//     //         Instantiate(bullet, transform.position, Quaternion.identity);
-//     //     } */
-//     // }
-    
-//     void Start()
-//     {
-//         rb = GetComponent<Rigidbody2D>();
-//     }
-
-//     void Update()
-//     {
-//         // Now just use rb here without calling GetComponent again
-//     }
-//     private void HandleMovement()
-//     {
-//         if (v.x != 0){
-//             animator.SetBool("isRunningFront", false)
-//             animator.SetBool("isRunningBack", false)
-//             animator.SetBool("isRunningX", true)
-
-//         }
-//         else if (v.y >0) {
-//             animator.SetBool("isRunningX", false)
-//             animator.SetBool("isRunningBack", false)
-//             animator.SetBool("isRunningFront", true)
-//         }
-//         else if (v.y <0) {
-//             animator.SetBool("isRunningFront", false)
-//             animator.SetBool("isRunningX", false)
-//             animator.SetBool("isRunningBack", true)
-//         }
-//         else if (v.y = 0) {
-//             animator.SetBool("isRunningX", false)
-//             animator.SetBool("isRunningFront", false)
-//             animator.SetBool("isRunningBack", false)
-//         }
-//     }
-// }   
-
-
-
-
-
-
-
-
 using UnityEngine;
 
 // IMPORTANT: Ensure the filename in Unity is "PlayerMovement.cs"
@@ -82,7 +9,7 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Movement Settings")]
     [SerializeField] private float moveSpeed = 5f;
-    
+
     private Vector2 moveInput;
 
     // Start is called before the first frame update
@@ -92,7 +19,7 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
 
         // Optional: Auto-fill animator if you forgot to drag it in the inspector
-        if (animator == null) 
+        if (animator == null)
         {
             animator = GetComponent<Animator>();
         }
@@ -137,13 +64,11 @@ public class PlayerMovement : MonoBehaviour
             // Flip sprite: 1 for right, -1 for left
             transform.localScale = new Vector3(Mathf.Sign(moveInput.x), 1, 1);
         }
-        // 2. Vertical (Y) - FIXED LOGIC HERE
+        // 2. Vertical (Y)
         else if (moveInput.y != 0)
         {
             animator.SetBool("isRunningX", false);
-            
-            // If moving UP (Y > 0), show BACK. If moving DOWN (Y < 0), show FRONT.
-            animator.SetBool("isRunningBack", moveInput.y > 0); 
+            animator.SetBool("isRunningBack", moveInput.y > 0);
             animator.SetBool("isRunningFront", moveInput.y < 0);
         }
         // 3. Idle
